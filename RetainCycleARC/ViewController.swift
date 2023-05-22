173f45e66f72444184bd7f5e90cb75aff9f0bcd6
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-22 20:38:28 +0300

    finished

--- RetainCycleARC/ViewController.swift
+++ RetainCycleARC/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var sean: Person?
+    var matilda: MacBook?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createObjects()
+        
     }
-
-
+    
+    func createObjects() {
+        sean = Person(name: "Sean", macbook: nil)
+        matilda = MacBook(name: "Matilda", owner: nil)
+    }
+    
+    func assignProperties() {
+        sean?.macbook = matilda
+        matilda?.owner = sean
+        
+        sean = nil
+        matilda = nil
+    }
 }
 
